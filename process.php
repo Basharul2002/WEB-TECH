52cea6df51f2baf5ccf63fa82079d997e7a30562
basharul2002 <basharulalam6@gmail.com>
2024-11-11 13:25:10 +0600

    Lab 3: Name, Email, date validation

--- process.php
+++ process.php
@@ -7,7 +7,8 @@
 </head>
 <body>
     <?php
-       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    { 
         // Retrieve form data
         $studentName = $_POST['userName'];
         $studentID = $_POST['userID'];
@@ -16,17 +17,50 @@
         $token = $_POST['token'];
         $returnDate = $_POST['returnDate'];
         $fees = $_POST['fees'];
-        $paid = $_POST['paid']
+        $paid = $_POST['paid'];
     
+        if (!preg_match("/[A-Za-z\-]/", $_POST['userName'])) 
+        { 
+            echo "Invalid Name";
+            return;
+        }
+        
+        if (!preg_match("/\d{2}-\d{5}-\d{1}/", $studentID)) 
+        {
+            echo "Invalid UserID";
+            return;
+        }
+
+        if (!preg_match("/[0-9]/", $_POST['fees'])) 
+        { 
+            echo "Invalid Name";
+            return;
+        }
+
+        
+        // Calculate the difference between the borrow date and return date
+        $interval = date_diff(new DateTime($borrowDate), new DateTime($returnDate));
+
+        // Check if the interval is greater than 10 days
+        if ($interval->format('%a') > 10) 
+        {
+            echo "Not able to borrow book for more than 10 days.";
+            return;
+        }
+
+        
+        
+
+
         // Display the submitted data
         echo "<h2>Form Submission Details:</h2>";
-        echo "Student Full Name: " . htmlspecialchars($studentName) . "<br>";
-        echo "Student ID: " . htmlspecialchars($studentID) . "<br>";
-        echo "Book Title: " . htmlspecialchars($bookTitle) . "<br>";
-        echo "Borrow Date: " . htmlspecialchars($borrowDate) . "<br>";
-        echo "Token: " . htmlspecialchars($token) . "<br>";
-        echo "Return Date: " . htmlspecialchars($returnDate) . "<br>";
-        echo "Fees: $" . htmlspecialchars($fees) . "<br>";
+        echo "Student Full Name: " . $studentName . "<br>";
+        echo "Student ID: " . $studentID . "<br>";
+        echo "Book Title: " . $bookTitle . "<br>";
+        echo "Borrow Date: " . $borrowDate . "<br>";
+        echo "Token: " . $token . "<br>";
+        echo "Return Date: " . $returnDate . "<br>";
+        echo "Fees: $" . $fees . "<br>";
 
         return;
     } 
